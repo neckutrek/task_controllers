119f3d0655f91ff9606c613c568151b1878b9b0d
Robert Krug <robert.krug@oru.se>
2016-12-08 17:41:07 +0100

    added the collision checker in the avoidance task

--- hiqp_core/include/hiqp/tasks/task_avoid_collisions_sdf.h
+++ hiqp_core/include/hiqp/tasks/task_avoid_collisions_sdf.h
@@ -21,7 +21,7 @@
 
 #include <hiqp/hiqp_time_point.h>
 #include <hiqp/task_definition.h>
-
+#include <hiqp_collision_check/checker_base.h>
 #include <kdl/treefksolverpos_recursive.hpp>
 #include <kdl/treejnttojacsolver.hpp>
 
@@ -31,13 +31,13 @@ namespace hiqp
   {
     /*! \brief A struct holding Jacobian and end-effector point - used for forward kinematics.
      *  \author Robert Krug */  
-struct KinematicQuantities
-{
-  std::string frame_id_;
-  KDL::Jacobian ee_J_;
-  KDL::Frame ee_pose_;
-};
-//==============================================================================================
+    struct KinematicQuantities
+    {
+      std::string frame_id_;
+      KDL::Jacobian ee_J_;
+      KDL::Frame ee_pose_;
+    };
+    //==============================================================================================
     /*! \brief A task definition that allows avoidance of geometric primitives on the manipulator with the environment given as a SDF map.
      *  \author Robert Krug */  
     class TaskAvoidCollisionsSDF : public TaskDefinition {
@@ -65,14 +65,16 @@ struct KinematicQuantities
       void reset();
       /*! This function computes the kinematic quantities for a primitive and clears the kin_q vector before computing*/
       int primitiveForwardKinematics(std::vector<KinematicQuantities>& kin_q_list, const std::shared_ptr<geometric_primitives::GeometricPrimitive>& primitive, RobotStatePtr robot_state)const;
-   /*! Helper function which computes ee pose and Jacobian w.r.t. a given frame*/
+      /*! Helper function which computes ee pose and Jacobian w.r.t. a given frame*/
       int forwardKinematics(KinematicQuantities& kin_q, const KDL::JntArray& q)const;
 
       std::shared_ptr<KDL::TreeFkSolverPos_recursive>  fk_solver_pos_;
       std::shared_ptr<KDL::TreeJntToJacSolver>         fk_solver_jac_;
 
- std::vector<std::shared_ptr<geometric_primitives::GeometricPrimitive> >    primitives_;
- std::string root_frame_id_;
+      std::vector<std::shared_ptr<geometric_primitives::GeometricPrimitive> >    primitives_;
+      std::string root_frame_id_;
+      /*! Interface to the SDF map*/
+      std::shared_ptr<hiqp::CollisionCheckerBase> collision_checker_;
     };
 
   } // namespace tasks

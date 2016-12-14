b58705a4f603a819867934ddc7a92d08070a6f71
Robert Krug <robert.krug@oru.se>
2016-12-14 14:38:21 +0100

    tweaked point point test

--- hiqp_ros/servicecalls/playground/test_point_point_projection.sh
+++ hiqp_ros/servicecalls/playground/test_point_point_projection.sh
@@ -1,13 +1,13 @@
 #disable gravity in Gazebo - useful to avoid drifting due to imperfect velocity tracking control
 rosservice call /gazebo/set_physics_properties '{time_step: 0.001, max_update_rate: 1000, gravity: [0.0, 0.0, 0.0], ode_config: {auto_disable_bodies: false, sor_pgs_precon_iters: 0, sor_pgs_iters: 50, sor_pgs_w: 1.3, sor_pgs_rms_error_tol: 0.0, contact_surface_layer: 0.001, contact_max_correcting_vel: 100.0, cfm: 0.0, erp: 0.2,  max_contacts: 20} }'
 
-#rosservice call /yumi/hiqp_joint_velocity_controller/add_primitive \
-#"name: 'test_point'
-#type: 'point'
-#frame_id: 'gripper_r_base'
-#visible: true
-#color: [1.0, 0.0, 0.0, 1.0]
-#parameters: [0, 0, 0.15]"
+rosservice call /yumi/hiqp_joint_velocity_controller/add_primitive \
+"name: 'test_point'
+type: 'point'
+frame_id: 'gripper_r_base'
+visible: true
+color: [1.0, 0.0, 0.0, 1.0]
+parameters: [0, 0, 0.15]"
 
 rosservice call /yumi/hiqp_joint_velocity_controller/add_primitive \
 "name: 'target_point'
@@ -15,7 +15,7 @@ type: 'point'
 frame_id: 'yumi_body'
 visible: true
 color: [0.0, 0.0, 1.0, 1.0]
-parameters: [0.4, 0.0, -0.1]"
+parameters: [0.4, 0.0, 0.1]"
 
 rosservice call /yumi/hiqp_joint_velocity_controller/set_task \
 "name: 'test_point_target_point'

f1b545b42b51d860f7dd07d94dcb9f814264621d
Robert Krug <robert.krug@oru.se>
2016-12-14 10:25:10 +0100

    fixed yaml file for full avoidance

--- hiqp_ros/servicecalls/playground/test_point_plane_projection.sh
+++ hiqp_ros/servicecalls/playground/test_point_plane_projection.sh
@@ -2,12 +2,12 @@
 rosservice call /gazebo/set_physics_properties '{time_step: 0.001, max_update_rate: 1000, gravity: [0.0, 0.0, 0.0], ode_config: {auto_disable_bodies: false, sor_pgs_precon_iters: 0, sor_pgs_iters: 50, sor_pgs_w: 1.3, sor_pgs_rms_error_tol: 0.0, contact_surface_layer: 0.001, contact_max_correcting_vel: 100.0, cfm: 0.0, erp: 0.2,  max_contacts: 20} }'
 
 rosservice call /yumi/hiqp_joint_velocity_controller/add_primitive \
-"name: 'gripper_r_base_avoidance_point'
+"name: 'test_point'
 type: 'point'
 frame_id: 'gripper_r_base'
 visible: true
 color: [1.0, 0.0, 0.0, 0.6]
-parameters: [0.0, 0.0, 0.18]"
+parameters: [0.0, 0.0, 0.15]"
 
 rosservice call /yumi/hiqp_joint_velocity_controller/add_primitive \
 "name: 'target_point'
@@ -22,5 +22,5 @@ rosservice call /yumi/hiqp_joint_velocity_controller/set_task \
 priority: 3
 visible: 1
 active: 1
-def_params: ['TDefGeomProj', 'point', 'point', 'gripper_r_base_avoidance_point = target_point']
+def_params: ['TDefGeomProj', 'point', 'point', 'test_point = target_point']
 dyn_params: ['TDynFirstOrder', '1.0']"
